33b2266c400ab4e3f3c5163d5e63d65d20ee466c
Rodrigo Gonzaga <rrgonzaga@gmail.com>
2020-03-01 13:11:59 -0300

    Aula 2.6. Salvando o recurso Livro a partir de um POST
    2. Criando uma API RESTful
    - Aula 2.6. Salvando o recurso Livro a partir de um POST.

--- src/main/java/com/algaworks/socialbooks/controllers/LivrosController.java
+++ src/main/java/com/algaworks/socialbooks/controllers/LivrosController.java
@@ -1,9 +1,9 @@
 package com.algaworks.socialbooks.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +16,18 @@ import com.algaworks.socialbooks.repository.LivrosRepository;
  * @apiNote quando acessar http://localhost:8080/h2-console/, colocar jdbc:h2:mem:testdb para JDBC URL.
  */
 @RestController
+@RequestMapping("/livros")
 public class LivrosController {
 	
 	@Autowired
 	private LivrosRepository livrosRepository;
 	
-	@RequestMapping(value="/livros", method = RequestMethod.GET)
+	/**
+	 * Obtém todos os livros cadastrados
+	 * @implNote O método GET obtém um ou mais recursos
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		
 //		Livro l1 = new Livro("Rest Aplicado");
@@ -33,6 +39,16 @@ public class LivrosController {
 		
 		return livrosRepository.findAll();
 		
+	}
+	
+	/**
+	 * Criar um novo livro no BD
+	 * @implNote O método POST é responsável por criar um novo recurso
+	 * @param livro
+	 */
+	@RequestMapping(method=RequestMethod.POST)
+	public Livro salvar(@RequestBody Livro livro) {
+		return livrosRepository.save(livro);
 	}
 
 }
